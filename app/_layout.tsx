56fb6db2e2145121158c85de3faa15b62e9d2f7c
RaulBus <raul.bus@student.upt.ro>
2024-12-19 19:40:02 +0200

    Add initial app structure with Chat, Map, Home, and Account screens; implement NotFoundScreen and update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,64 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React, { useEffect } from 'react'
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function TabLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs
+ 
+
+      screenOptions={({ route }) => ({
+
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap;
+          
+          
+          const routeName = route.name.split('/').pop();
+            
+
+          switch (routeName) {
+            case 'index':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+            case 'map':
+              iconName = focused ? 'map' : 'map-outline';
+              break;
+            case 'chat':
+              iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+              break;
+            case 'settings':
+              iconName = focused ? 'settings' : 'settings-outline';
+              break;
+            default:
+                iconName = 'alert-circle';
+                break;
+          }
+
+            return <Ionicons name={iconName} size={size} color={color}  style={{alignContent: 'center', justifyContent: 'center' }} />;
+        },
+
+        
+        tabBarActiveTintColor: '#4CAF50',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+        tabBarPosition: 'bottom',
+        tabBarShowLabel: false,
+        tabBarStyle: {
+            width: '98%',
+            height: '8%',
+            margin: '1%',
+            borderRadius: 25,
+            backgroundColor: 'white',
+            position: 'absolute',
+            },
+        tabBarIconStyle: {
+            width: '100%',
+            height: '100%',
+            
+            },
+       headerShadowVisible: false,
+       
+      })}
+    />
   );
 }
